d0ce9d89615254b5545c92ab9f56cc72f9185c9b
Smaran Jawalkar <smaran.jawalkar@gmail.com>
2023-04-12 16:10:13 +0530

    bug fix: selects prefix now

--- src/SQLCompiler/SQLCompiler.c
+++ src/SQLCompiler/SQLCompiler.c
@@ -14,21 +14,10 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
     statement->type = STATEMENT_INSERT;
     return PREPARE_SUCCESS;
   }
-  if (strcmp(input_buffer->buffer, "select") == 0) {
+  if (strncmp(input_buffer->buffer, "select", 6) == 0) {
     statement->type = STATEMENT_SELECT;
     return PREPARE_SUCCESS;
   }
 
   return PREPARE_UNRECOGNIZED_STATEMENT;
-}
-
-void execute_statement(Statement* statement) {
-  switch (statement->type) {
-    case (STATEMENT_INSERT):
-      printf("This is where we would do an insert.\n");
-      break;
-    case (STATEMENT_SELECT):
-      printf("This is where we would do a select.\n");
-      break;
-  }
 }
